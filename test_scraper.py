996e5136b33ba9d4b5321c37fe7fcfe7c3ccd1a3
opensource-joe <joe.castle@sas.com>
2023-02-20 12:03:13 -0500

    updates to the scraper

--- test_scraper.py
+++ test_scraper.py
@@ -8,5 +8,16 @@ url = "https://www.actiac.org/upcoming-events"
 response = requests.get(url)
 
 # begin scraping w/ BeautifulSoup
-soup = BeautifulSoup(response.content)
-print(soup)
+soup = BeautifulSoup(response.content, features='html.parser')
+# print(soup)
+
+# identify elements by ID
+# results_id = soup.find(id='main')
+# print(results_id)
+
+# identify content by class name
+results_class = soup.findAll('div', class_='cards-item non-sticky views-row')
+print(results_class)
+
+# how many class='field-content'
+print(len(results_class))
