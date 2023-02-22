89d8c49581f447ba540dd291efc2faf3170b17bd
opensource-joe <joe.castle@sas.com>
2023-02-22 13:55:27 -0500

    adding updates

--- actiac_events.py
+++ actiac_events.py
@@ -10,8 +10,8 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, features='html.parser')
 
 # find all events on the page by event card with div and class
-css_class_card = soup.findAll('div', class_='cards-item non-sticky views-row')
-# print(css_class_card)
+cards = soup.findAll('div', class_='cards-item non-sticky views-row')
+# print(cards)
 
 # TODO: use class closest to text element to make sure it pulls correct text (e.g., div class="cards-description" then "p")
 
@@ -23,7 +23,6 @@ css_class_card = soup.findAll('div', class_='cards-item non-sticky views-row')
 # type of event - COI, Conference, FIE, Small Biz Alliance, Pro Dev
 # <img alt="" src="/sites/default/files/community_of_interest.png">
 
-# event_type = css_class_card[0].find('span', class_='field_content', html_='img')
 # event_type = css_class_card[0].find('img')
 # print(event_type)
 # TODO: get event_type from URL, format as needed for excel file
@@ -32,40 +31,40 @@ css_class_card = soup.findAll('div', class_='cards-item non-sticky views-row')
 # event format - virtual, in-person, in-person and virtual, unknown, multi-day event
 # <strong>Virtual Event</strong>
 
-event_format = css_class_card[0].find('strong')
-event_text_only = event_format.text
-print(event_text_only)
+# event_format = [events[0].find('strong').text for card in cards]
+# event_text_only = event_format.text
+# print(event_text_only)
 # TODO: loop for all cards
 
 # event title - like "ACT-IAC Health COI February 2023"
 # <h4>ACT-IAC Health COI February 2023</h4>
 
-event_title = css_class_card[0].find('h4')
-event_title_only = event_title.text
-print(event_title_only)
+# event_title = css_class_card[0].find('h4')
+# event_title_only = event_title.text
+# print(event_title_only)
 # TODO: loop for all cards
 
 # event date - like "Tuesday, February 21, 2023"
 # <time datetime="00Z" class="datetime">Tuesday, February 21, 2023</time>
 
-event_date = css_class_card[0].find('time')
-event_date_only = event_date.text
-print(event_date_only)
+# event_date = css_class_card[0].find('time')
+# event_date_only = event_date.text
+# print(event_date_only)
 # TODO: loop for all cards
 
 # event description
 # <p>Join the ACT-IAC Health COI meeting to hear from...</p>
 
-event_description = css_class_card[0].find('p')
-event_description_only = event_description.text
-print(event_description_only)
+# event_description = css_class_card[0].find('p')
+# event_description_only = event_description.text
+# print(event_description_only)
 # TODO: loop for all cards
 
 # Learn More button - link to event page
 # <a href="/act-iac-event/act-iac-health-coi-february-2023">Learn More</a>
 
-event_more = css_class_card[0].find('a')
-print(event_more)
+# event_more = css_class_card[0].find('a')
+# print(event_more)
 # event_more_only = event_more.text
 # print(event_more_only)
 # TODO: determine how to strip URLs from an href
