f3ed9661eebd56fa967ebd4e94493560a996e334
opensource-joe <joe.castle@sas.com>
2023-03-01 14:49:49 -0500

    finished v1 w/ parser, working on formatting

--- act-iac/projects.py
+++ act-iac/projects.py
@@ -13,23 +13,61 @@ cards = soup.find_all('div', {'class': 'view-content row'})[0]
 
 # ---------------------
 
-# find card title for seeking volunteers
+# Seeking Volunteers
 
 cards_lev2 = cards.find_all('div', {'class', 'views-row'})[0]
+cards_lev3 = cards_lev2.find_all('div', {'class', 'views-row'})
 
-# seek_cards = cards_lev2.find_all('span', {'field-content'})
-# seek_cards = [getattr(title.find('span'), 'text', None) for title in cards_lev2]
+# header
+accord_group_seek = [cards_lev2.find('h3').text]
 
-for card in cards_lev2:
-    print(card)
+# card title
+card_title = [getattr(title.find('span'), 'text', None) for title in cards_lev3]
 
+# click to learn more
 
-# find card title for in progress
+cards_link = cards_lev2.find_all('a')
 
+card_link_prep = []
+for a in cards_link:
+    if 'https' in str(a):
+        card_link_prep.append("invalid_link")
+    else:
+        card_link_prep.append(a)
+
+card_link_final = []
+for link in card_link_prep:
+    if link != "invalid_link":
+        card_link_final.append(f"https://www.actiac.org" + link.get('href'))
 
 # ---------------------
 
-# COI title, only need text, href controls accordion effect
-# <a href="/groups/acquisition-community-interest" hreflang="en">Acquisition Community of Interest</a>
+# In Progress
 
-# coi_title = [getattr(title.find('a'), 'text', None) for title in post_cards]
+# bumping up variable to 10th to capture all In Progress cards
+cards_lev10 = cards.find_all('div', {'class', 'views-row'})[4]
+cards_lev11 = cards_lev10.find_all('div', {'class', 'views-row'})
+
+# header
+
+accord_group_prog = [cards_lev10.find('h3').text]
+
+# card title
+
+card_title_prog = [getattr(title.find('span'), 'text', None) for title in cards_lev11]
+
+# click to learn more
+
+cards_link_prog = cards_lev10.find_all('a')
+
+card_link_prep_prog = []
+for a in cards_link_prog:
+    if 'https' in str(a):
+        card_link_prep_prog.append("invalid_link")
+    else:
+        card_link_prep_prog.append(a)
+
+card_link_prog_final = []
+for link in card_link_prep_prog:
+    if link != "invalid_link":
+        card_link_prog_final.append(f"https://www.actiac.org" + link.get('href'))
--- act-iac/write_to_excel_start_here.py
+++ act-iac/write_to_excel_start_here.py
@@ -1,7 +1,7 @@
 import xlsxwriter
 import events
 import cois
-# import projects
+import projects
 
 # ---------------------
 
@@ -9,13 +9,19 @@ workbook = xlsxwriter.Workbook('act-iac/ACT-IAC_Events-03.01.23.xlsx')
 
 # ---------------------
 
+# formatting
+
+bold = workbook.add_format({'bold': True})
+
+# ---------------------
+
 # report content for Events
 
-worksheet1 = workbook.add_worksheet('Events_Calendar')
+worksheet1 = workbook.add_worksheet('Events Calendar')
 
-header1 = ['Event_Type', 'Event_Format', 'Event_Title', 'Event_Date', 'Description', 'Learn More']
+header1 = ['Event Type', 'Event Format', 'Event Title', 'Event Date', 'Description', 'Learn More']
 
-worksheet1.write_row('A1', header1)
+worksheet1.write_row('A1', header1, bold)
 worksheet1.write_column('A2', events.event_type)
 worksheet1.write_column('B2', events.event_format)
 worksheet1.write_column('C2', events.event_title)
@@ -29,9 +35,9 @@ worksheet1.write_column('F2', events.event_more_2)
 
 worksheet2 = workbook.add_worksheet('COIs')
 
-header2 = ['COI_Title', 'Description', 'COI_Page']
+header2 = ['COI Title', 'Description', 'COI Page']
 
-worksheet2.write_row('A1', header2)
+worksheet2.write_row('A1', header2, bold)
 worksheet2.write_column('A2', cois.coi_title)
 worksheet2.write_column('B2', cois.coi_description)
 worksheet2.write_column('C2', cois.coi_link_2)
@@ -40,12 +46,21 @@ worksheet2.write_column('C2', cois.coi_link_2)
 
 # report content for Projects
 
-# worksheet3 = workbook.add_worksheet('Projects')
+worksheet3 = workbook.add_worksheet('Projects')
 
-# header3 = ['Project_Title']
+worksheet3.write_row('A1', projects.accord_group_seek, bold)
+header3_1 = ('Project Title', 'Learn More')
 
-# worksheet3.write_row('A1', header3)
-# worksheet3.write_column('A2', [tbd])
+worksheet3.write_row('A3', header3_1, bold)
+worksheet3.write_column('A4', projects.card_title)
+worksheet3.write_column('B4', projects.card_link_final)
+
+worksheet3.write_row('A10', projects.accord_group_prog, bold)
+header3_2 = ('Project Title', 'Learn More')
+
+worksheet3.write_row('A11', header3_2, bold)
+worksheet3.write_column('A12', projects.card_title_prog)
+worksheet3.write_column('B12', projects.card_link_prog_final)
 
 # ---------------------
 
