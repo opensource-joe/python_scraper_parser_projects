2ec2430ecc576463ccfdfcbae4b5a1ec0dba0d98
opensource-joe <joe.castle@sas.com>
2023-05-05 15:39:59 -0400

    final script w/ printing

--- act-iac/emergtech23.py
+++ act-iac/emergtech23.py
@@ -1,4 +1,10 @@
+import xlsxwriter
 from bs4 import BeautifulSoup
+
+# ---------------------
+
+workbook = xlsxwriter.Workbook('act-iac/EmergingTech_2023.xlsx')
+worksheet1 = workbook.add_worksheet('Speakers')
 
 # ---------------------
 
@@ -8,23 +14,8 @@ contents = open_file.read()
 soup = BeautifulSoup(contents, 'html.parser')
 cards = soup.find('div', {'class', 'SpeakersStyles__gridContainerGrouping___eiwPT grid__container___J52Tg'})
 
-# print(cards)
-
-# ---------------------
-
-# keynote speaker
-
-# TODO: image src from website
-
-# img
-# keynote_speaker_img = cards.findAll('img')
-# for image in speaker_img:
-#     print(image['src'])
-
-# ---
-
-# keynote speaker text values
-
+# ---- data source ----
+# keynote 
 keynote_speaker = cards.findAll('div')[4]
 keynote_speaker2 = keynote_speaker.findAll('span')
 
@@ -32,52 +23,7 @@ keynote_speaker_list = []
 for item in keynote_speaker2:
     keynote_speaker_list.append(item.text.strip())
 
-# ---
-
-# first name
-
-keynote_first_name = []
-for first in keynote_speaker_list[::5]:
-    keynote_first_name.append(first)
-
-# ---
-
-# last name
-
-keynote_last_name = []
-for last in keynote_speaker_list[1::5]:
-    keynote_last_name.append(last)
-
-# ---
-
-# title
-
-keynote_title = []
-for value_title in keynote_speaker_list[2::5]:
-    keynote_title.append(value_title)
-
-# ---
-
-# agency
-
-keynote_agency = []
-for value_agency in keynote_speaker_list[3::5]:
-    keynote_agency.append(value_agency)
-
-# ---
-
-# TODO: profile
-
-# ---------------------
-
-# speakers text values
-
-# TODO: image src from url
-
-# ---
-
-# speaker text values
-
+# speaker 
 speaker = cards.findAll('div')[56]
 speaker2 = speaker.findAll('span')
 
@@ -85,86 +31,195 @@ speaker_list = []
 for item in speaker2:
     speaker_list.append(item.text.strip())
 
-# ---
+# moderator 
+moderator = cards.findAll('div')[391]
+moderator2 = moderator.findAll('span')
 
-# first name
+moderator_list = []
+for item in moderator2:
+    moderator_list.append(item.text.strip())
 
+# govt cochair 
+govt = cards.findAll('div')[500]
+govt2 = govt.findAll('span')
+
+govt_cochair_list = []
+for item in govt2:
+    govt_cochair_list.append(item.text.strip())
+
+# industry cochair 
+industry = cards.findAll('div')[530]
+industry2 = industry.findAll('span')
+
+industry_cochair_list = []
+for item in industry2:
+    industry_cochair_list.append(item.text.strip())
+
+# advisor 
+advisor = cards.findAll('div')[550]
+advisor2 = advisor.findAll('span')
+
+advisor_cochair_list = []
+for item in advisor2:
+    advisor_cochair_list.append(item.text.strip())
+
+# ---- first name ----
+# keynote 
+keynote_first_name = []
+for first in keynote_speaker_list[::5]:
+    keynote_first_name.append(first)
+
+# speaker 
 speaker_first_name = []
 for first in speaker_list[::5]:
     speaker_first_name.append(first)
 
-# last name
+# moderator 
+moderator_first_name = []
+for first in moderator_list[::5]:
+    moderator_first_name.append(first)
 
+# govt cochair 
+govt_first_name = []
+for first in govt_cochair_list[::5]:
+    govt_first_name.append(first)
+
+# industry cochair 
+industry_first_name = []
+for first in industry_cochair_list[::5]:
+    industry_first_name.append(first)
+
+# advisor
+advisor_first_name = []
+for first in advisor_cochair_list[::5]:
+    advisor_first_name.append(first)
+
+# ---- last name ----
+# keynote 
+keynote_last_name = []
+for last in keynote_speaker_list[1::5]:
+    keynote_last_name.append(last)
+    
+# speaker 
 speaker_last_name = []
 for last in speaker_list[1::5]:
     speaker_last_name.append(last)
+    
+# moderator 
+moderator_last_name = []
+for last in moderator_list[1::5]:
+    moderator_last_name.append(last)
 
+# govt cochair 
+govt_last_name = []
+for last in govt_cochair_list[1::5]:
+    govt_last_name.append(last)
+    
+# industry cochair 
+industry_last_name = []
+for last in industry_cochair_list[1::5]:
+    industry_last_name.append(last)
 
-# ---
+# advisor
+advisor_last_name = []
+for last in advisor_cochair_list[1::5]:
+    advisor_last_name.append(last)
 
-# title
-
+# ---- title ----
+# keynote 
+keynote_title = []
+for value_title in keynote_speaker_list[2::5]:
+    keynote_title.append(value_title)
+    
+# speaker 
 speaker_title = []
 for value_title in speaker_list[2::5]:
     speaker_title.append(value_title)
+    
+# moderator 
+moderator_title = []
+for value_title in moderator_list[2::5]:
+    moderator_title.append(value_title)
 
-# ---
+# govt cochair 
+govt_title = []
+for value_title in govt_cochair_list[2::5]:
+    govt_title.append(value_title)
+    
+# industry cochair 
+industry_title = []
+for value_title in industry_cochair_list[2::5]:
+    industry_title.append(value_title)
 
-# agency
+# advisor
+advisor_title = []
+for value_title in advisor_cochair_list[2::5]:
+    advisor_title.append(value_title)
 
+# ---- organization ----
+# keynote 
+keynote_agency = []
+for value_agency in keynote_speaker_list[3::5]:
+    keynote_agency.append(value_agency)
+    
+# speaker 
 speaker_agency = []
 for value_agency in speaker_list[3::5]:
     speaker_agency.append(value_agency)
+    
+# moderator 
+moderator_agency = []
+for value_agency in moderator_list[3::5]:
+    moderator_agency.append(value_agency)
 
-# ---
+# govt cochair 
+govt_agency = []
+for value_agency in govt_cochair_list[3::5]:
+    govt_agency.append(value_agency)
+    
+# industry cochair 
+industry_agency = []
+for value_agency in industry_cochair_list[3::5]:
+    industry_agency.append(value_agency)
 
-# TODO: profile
+# advisor
+advisor_agency = []
+for value_agency in advisor_cochair_list[3::5]:
+    advisor_agency.append(value_agency)
 
 # ---------------------
 
-# moderators text values
+header1 = ['First_Name', 'Last_Name', 'Title', 'Organization']
+bold = workbook.add_format({'bold': True})
 
-# TODO: image src from url
+worksheet1.write_row('A1', header1, bold)
 
-# ---
+worksheet1.write_column('A2', keynote_first_name)
+worksheet1.write_column('A7', speaker_first_name)
+worksheet1.write_column('A43', moderator_first_name)
+worksheet1.write_column('A54', govt_first_name)
+worksheet1.write_column('A56', industry_first_name)
+worksheet1.write_column('A58', advisor_first_name)
 
-# moderators text values
+worksheet1.write_column('B2', keynote_last_name)
+worksheet1.write_column('B7', speaker_last_name)
+worksheet1.write_column('B43', moderator_last_name)
+worksheet1.write_column('B54', govt_last_name)
+worksheet1.write_column('B56', industry_last_name)
+worksheet1.write_column('B58', advisor_last_name)
 
-moderators = cards.findAll('div')[500]
-moderators2 = moderators.findAll('span')
+worksheet1.write_column('C2', keynote_title)
+worksheet1.write_column('C7', speaker_title)
+worksheet1.write_column('C43', moderator_title)
+worksheet1.write_column('C54', govt_title)
+worksheet1.write_column('C56', industry_title)
+worksheet1.write_column('C58', advisor_title)
 
-moderators_list = []
-for item in moderators2:
-    moderators_list.append(item.text.strip())
+worksheet1.write_column('D2', keynote_agency)
+worksheet1.write_column('D7', speaker_agency)
+worksheet1.write_column('D43', moderator_agency)
+worksheet1.write_column('D54', govt_agency)
+worksheet1.write_column('D56', industry_agency)
+worksheet1.write_column('D58', advisor_agency)
 
-# ---
-
-# first name
-
-moderator_first_name = []
-for first in moderators_list[::5]:
-    moderator_first_name.append(first)
-    
-# ---
-
-# last name
-
-moderator_last_name = []
-for last in moderators_list[1::5]:
-    moderator_last_name.append(last)
-
-# ---
-
-# title
-
-moderator_title = []
-for value_title in moderators_list[2::5]:
-    moderator_title.append(value_title)
-
-# ---
-
-# agency
-
-moderator_agency = []
-for value_agency in moderators_list[3::5]:
-    moderator_agency.append(value_agency)
+workbook.close()
